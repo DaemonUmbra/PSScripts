2307210358d88056dfb04c7969d9deddef1d5824
DaemonUmbra <daemon.umbra@gmail.com>
2019-10-29 20:18:28 -0400

    Added Mass Git Fetch.ps1

---
+++ Mass Git Fetch.ps1
@@ -0,0 +1,10 @@
+﻿Get-ChildItem -Directory -Recurse -Depth 2 | ForEach-Object {
+Write-Debug("Checking " + $_.FullName)
+if(Test-Path ($_.FullName + "/.git")) {
+Write-Host("Found Git Repo: " + $_.Name + ", fetching...")
+Push-Location $_.FullName
+git fetch --all
+Write-Host("Finished Fetching " + $_.Name + [Environment]::NewLine)
+Pop-Location
+}
+}
